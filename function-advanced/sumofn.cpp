4630388084b94f7e9168ff9ffe1b2bdc5b137275
indranil2507 <iindranilsaha1@gmail.com>
2021-01-31 21:16:06 +0530

    ...

---
+++ function-advanced/sumofn.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int sum(int n){
+    int add =0;
+    return add= (n*(n+1))/2;
+}
+
+ int main(){
+     int n;
+      cin>>n;
+
+      cout<<sum(n)<<endl;
+
+
+ }
