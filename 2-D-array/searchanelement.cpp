d5979f763f12cc78b46711cb393f995abb081023
indranil2507 <iindranilsaha1@gmail.com>
2021-05-07 01:18:27 +0530

    2-d array

---
+++ 2-D-array/searchanelement.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int arr[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
+        cout << endl;
+    }
+
+    cout << "the matrix is" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    int x;
+    cin >> x;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (arr[i][j] == x)
+                cout << "element in " << i << "th row and " << j << "th column";
+        }
+        cout << endl;
+    }
+}
--- array/sumsub.cpp
+++ array/sumsub.cpp
@@ -11,16 +11,19 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-
+    int maxSum=0;
     int curr = 0;
     for(int i=0;i<n;i++){
        curr=0;
        for(int j=i;j<n;j++){
            curr+=arr[j];
-           cout<<curr<<endl;
+           // cout<<curr<<endl;
 
        }
+       maxSum= max(maxSum,curr);
+
     }
+    cout<<maxSum<<endl;
 
     return 0;
     
